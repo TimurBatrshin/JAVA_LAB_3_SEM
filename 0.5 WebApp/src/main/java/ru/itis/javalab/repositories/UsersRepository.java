7f899dd9f7eb9b0c489017e430315bc7c6200070
Timur <batrshintimur@yandex.ru>
2021-01-21 19:03:02 +0300

    Create UsersRepository.java

---
+++ 0.5 WebApp/src/main/java/ru/itis/javalab/repositories/UsersRepository.java
@@ -0,0 +1,11 @@
+package ru.itis.javalab.repositories;
+
+import ru.itis.javalab.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsersRepository extends CrudRepository<User>{
+    List<User> findAllByAge(Integer age);
+    Optional<User> findFirstByFirstNameAndLastName(String firstName, String lastName);
+}
