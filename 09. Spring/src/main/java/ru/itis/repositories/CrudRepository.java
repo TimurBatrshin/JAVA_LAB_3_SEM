0ded566db25bd2e6f4667ec6e00ddf388336542b
Timur <batrshintimur@yandex.ru>
2021-01-23 04:23:25 +0300

    Create CrudRepository.java

---
+++ 09. Spring/src/main/java/ru/itis/repositories/CrudRepository.java
@@ -0,0 +1,12 @@
+package ru.itis.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<T, ID> {
+    void save(T entity);
+    void delete(ID id);
+    void update(T entity);
+    Optional<T> findById(ID id);
+    List<T> findAll();
+}
