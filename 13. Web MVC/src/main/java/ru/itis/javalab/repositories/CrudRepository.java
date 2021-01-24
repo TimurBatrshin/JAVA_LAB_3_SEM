9304c24be414b31f2d5321ef7acc822eeddf9768
Timur <batrshintimur@yandex.ru>
2021-01-24 13:19:41 +0300

    Create CrudRepository.java

---
+++ 13. Web MVC/src/main/java/ru/itis/javalab/repositories/CrudRepository.java
@@ -0,0 +1,24 @@
+package ru.itis.javalab.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 08.10.2020
+ * 05. WebApp
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public interface CrudRepository<T> {
+    List<T> findAll();
+    List<T> findAll(int page, int size);
+    Optional<T> findById(Long id);
+
+    void save(T entity);
+    void update(T entity);
+    void deleteById(Long id);
+    void delete(T entity);
+
+
+}
