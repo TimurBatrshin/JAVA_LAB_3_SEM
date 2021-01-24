d16c9ab9cbfb9f3c32d0dbb0a1be0bf8ac3c3c80
Timur <batrshintimur@yandex.ru>
2021-01-24 13:12:47 +0300

    Update UsersService.java

--- 0.5 WebApp/src/main/java/ru/itis/javalab/services/UsersService.java
+++ 0.5 WebApp/src/main/java/ru/itis/javalab/services/UsersService.java
@@ -1,5 +1,6 @@
 package ru.itis.javalab.services;
 
+import ru.itis.javalab.dto.UserDto;
 import ru.itis.javalab.models.User;
 
 import java.util.List;
@@ -7,4 +8,8 @@ import java.util.Optional;
 
 public interface UsersService {
     List<User> getAllUsers();
+    List<UserDto> getAllUsers(int page, int size);
+    void addUser(UserDto userDto);
+
+    void deleteUserById(Long id);
 }
