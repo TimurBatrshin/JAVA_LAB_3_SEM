a99ece22a289c10259738d05f73500a4fa8c8c2f
Timur <batrshintimur@yandex.ru>
2021-01-24 13:11:10 +0300

    Create UserDto.java

---
+++ 0.5 WebApp/src/main/java/ru/itis/javalab/dto/UserDto.java
@@ -0,0 +1,32 @@
+package ru.itis.javalab.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.itis.javalab.models.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private String firstName;
+    private String lastName;
+
+    public static UserDto from(User user){
+        return UserDto.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .build();
+    }
+
+    public  static List<UserDto> from(List<User> users) {
+        return users.stream()
+                .map(UserDto::from)
+                .collect(Collectors.toList());
+    }
+}
