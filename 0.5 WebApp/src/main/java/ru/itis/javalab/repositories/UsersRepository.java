628000486e4fe9b45a38accd31a5d81a4768a3be
Timur <batrshintimur@yandex.ru>
2021-01-24 13:13:01 +0300

    Update UsersRepository.java

--- 0.5 WebApp/src/main/java/ru/itis/javalab/repositories/UsersRepository.java
+++ 0.5 WebApp/src/main/java/ru/itis/javalab/repositories/UsersRepository.java
@@ -9,4 +9,6 @@ public interface UsersRepository extends CrudRepository<User>{
     List<User> findAllByAge(Integer age);
     Optional<User> findFirstByFirstNameAndLastName(String firstName, String lastName);
     Optional<User> findOneById(Long id);
+    List<User> updateUser(Long id);
+
 }
