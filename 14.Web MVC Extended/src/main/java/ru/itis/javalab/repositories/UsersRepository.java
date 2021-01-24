abdc341224ca6c623eb091ac601d7289ba75936f
Timur <batrshintimur@yandex.ru>
2021-01-24 13:21:06 +0300

    Create UsersRepository.java

---
+++ 14.Web MVC Extended/src/main/java/ru/itis/javalab/repositories/UsersRepository.java
@@ -0,0 +1,11 @@
+package ru.itis.javalab.repositories;
+
+import ru.itis.javalab.models.User;
+
+import java.util.Optional;
+
+public interface UsersRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findFirstByEmailAndPassword(String email, String pass);
+    Optional<User> findUserByAge(int age);
+}
