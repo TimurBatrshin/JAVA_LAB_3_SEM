e525a28e44bb16d30455a14fef706c2fa1955609
Timur <batrshintimur@yandex.ru>
2021-01-24 13:20:35 +0300

    Create ApplicationConfig.java

---
+++ 14.Web MVC Extended/src/main/java/ru/itis/javalab/config/ApplicationConfig.java
@@ -0,0 +1,86 @@
+package ru.itis.javalab.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+import ru.itis.javalab.repositories.UsersRepository;
+import ru.itis.javalab.repositories.UsersRepositoryJdbcTemplateImpl;
+import ru.itis.javalab.services.*;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("ru.itis.javalab")
+@PropertySource("classpath:db.properties")
+public class ApplicationConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public BCrypterService bCrypterService() {
+        return new BCrypterServiceImpl();
+    }
+
+    @Bean
+    public UsersService usersService() {
+        return new UsersServiceImpl(usersRepository());
+    }
+
+    @Bean
+    public UsersRepository usersRepository() {
+        return new UsersRepositoryJdbcTemplateImpl(jdbcTemplate());
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate jdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public HikariConfig hikariConfig() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(environment.getProperty("db.url"));
+        hikariConfig.setMaximumPoolSize(Integer.parseInt(environment.getProperty("db.hikari.max-pool-size")));
+        hikariConfig.setUsername(environment.getProperty("db.username"));
+        hikariConfig.setPassword(environment.getProperty("db.password"));
+        hikariConfig.setDriverClassName(environment.getProperty("db.driver-classname"));
+        return hikariConfig;
+    }
+
+    @Bean
+    public FreeMarkerViewResolver freeMarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftlh");
+        resolver.setContentType("text/html;charset=UTF-8");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfig() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("/WEB-INF/ftl");
+        return configurer;
+    }
+
+}
