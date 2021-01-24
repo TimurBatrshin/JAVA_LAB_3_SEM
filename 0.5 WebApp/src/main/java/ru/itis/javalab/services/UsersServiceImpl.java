768c06a117d7046d554cfef26f02407b9068169b
Timur <batrshintimur@yandex.ru>
2021-01-24 13:12:40 +0300

    Update UsersServiceImpl.java

--- 0.5 WebApp/src/main/java/ru/itis/javalab/services/UsersServiceImpl.java
+++ 0.5 WebApp/src/main/java/ru/itis/javalab/services/UsersServiceImpl.java
@@ -1,9 +1,12 @@
 package ru.itis.javalab.services;
 
+import ru.itis.javalab.dto.UserDto;
 import ru.itis.javalab.models.User;
 import ru.itis.javalab.repositories.UsersRepository;
 
 import java.util.List;
+
+import static ru.itis.javalab.dto.UserDto.from;
 
 public class UsersServiceImpl implements UsersService {
 
@@ -16,5 +19,29 @@ public class UsersServiceImpl implements UsersService {
     @Override
     public List<User> getAllUsers() {
         return usersRepository.findAll();
+    }
+
+    @Override
+    public List<UserDto> getAllUsers(int page, int size) {
+        return  from(usersRepository.findAll(page, size));
+    }
+
+    @Override
+    public void addUser(UserDto userDto) {
+        usersRepository.save(User.builder()
+        .age(null)
+        .firstName(userDto.getFirstName())
+        .lastName(userDto.getLastName())
+                .build());
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        usersRepository.findOneById(id).ifPresent(
+                user -> {
+                    user.setIsDeleted(true);
+                    usersRepository.update(user);
+                }
+        );
     }
 }
