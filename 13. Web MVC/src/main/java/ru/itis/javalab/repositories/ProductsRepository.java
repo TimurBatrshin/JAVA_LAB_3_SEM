dc58751385069c98bff9114571c407940eb7b05e
Timur <batrshintimur@yandex.ru>
2021-01-24 13:19:44 +0300

    Create ProductsRepository.java

---
+++ 13. Web MVC/src/main/java/ru/itis/javalab/repositories/ProductsRepository.java
@@ -0,0 +1,16 @@
+package ru.itis.javalab.repositories;
+
+import ru.itis.javalab.models.Product;
+import ru.itis.javalab.models.User;
+
+import java.util.List;
+
+/**
+ * 08.10.2020
+ * 05. WebApp
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public interface ProductsRepository extends CrudRepository<User> {
+}
